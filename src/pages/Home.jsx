ba7ff290561ecba835a3c242b54235c96c0ea08e
mahmudul35 <mahmudul.cse.pust@gmail.com>
2024-12-31 14:58:06 +0600

    create category swiper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Banner } from './Home/Banner/Banner'
+import { Category } from './Home/Category/Category'
 
 export const Home = () => {
   return (
     <div>
       <Banner/>
+      <Category/>
     </div>
   )
 }
